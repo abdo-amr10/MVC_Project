3ae8b164275192aaa6fdcb96d2fb9cb6e954a411
abdo-amr10 <amr838836@gmail.com>
2025-03-17 13:43:17 +0200

    Add project files.

---
+++ Demp.PL/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+namespace Demp.PL.ViewModels
+
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
