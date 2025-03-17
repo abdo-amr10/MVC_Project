5d871d7968f68a8d42333f922461d1ae630829b5
abdo-amr10 <amr838836@gmail.com>
2025-03-17 15:39:53 +0200

    Part 03 Department Module (Entities, Configurations) and DbContext

---
+++ Demo.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.DAL.Entities.Departments;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.DAL.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer
+                ( "Server = . ; Database = MVCProject ; Trusted_Connection = true ; TrustServerCertificate = true ");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            public DbSet<Department> Departments { get; set; }
+        }
+    }
+}
---
+++ Demo.DAL/Entities/Departments/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Entities.Departments
+{
+    public class Department : BaseModel
+    {
+        public string Name { get; set; } = null!;
+        public string Code { get; set; } = null!;
+        public string? Description { get; set; }
+        public DateOnly CreationDate { get; set; }
+
+
+    }
+}
